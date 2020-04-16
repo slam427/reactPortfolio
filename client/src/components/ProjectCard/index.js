88212a2fe00f4b9336af6e372d8fa72a5792b119
Steven Lam <stevenvl@gmail.com>
2020-04-16 18:34:41 -0400

    image sources fixed

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import PasswordGen from '../../Assets/PasswordGen.png';
+import UnBoxed from '../../Assets/unboxed.png';
+import Gproject1 from '../../Assets/GroupProject1.png';
+import TeamBuilder from '../../Assets/EmployeeGenerator.png';
 
 function ProjectCard() {
     return (
         <div>
             <div className="card" style={{width: "36rem"}}>
-                <img src="../PasswordGen.png" className="card-img-top" alt="password generator site" />
+                <img src={PasswordGen} className="card-img-top" alt="password generator site" />
                 <div className="card-body">
                     <h5 className="card-title">Password Generator</h5>
                     <p>Generate a psuedo-random password</p>
@@ -13,7 +17,7 @@ function ProjectCard() {
             </div>
 
             <div className="card" style={{width: "36rem"}}>
-                <img src="/unboxed.png" className="card-img-top" alt="boardgame database" />
+                <img src={UnBoxed} className="card-img-top" alt="boardgame database" />
                 <div className="card-body">
                     <h5 className="card-title">Unboxed</h5>
                     <p>Board game database</p>
@@ -22,7 +26,7 @@ function ProjectCard() {
             </div>
 
             <div className="card" style={{width: "36rem"}}>
-                <img src="/GroupProject1.png" className="card-img-top" alt="Event search" />
+                <img rc={Gproject1} className="card-img-top" alt="Event search" />
                 <div className="card-body">
                     <h5 className="card-title">What's Happening Philly</h5>
                     <p>Search for events in Philadelphia</p>
@@ -31,7 +35,7 @@ function ProjectCard() {
             </div>
 
             <div className="card" style={{width: "36rem"}}>
-                <img src="/EmployeeGenerator.png" className="card-img-top" alt="developer team builder" />
+                <img rc={PasswordGen} className="card-img-top" alt="developer team builder" />
                 <div className="card-body">
                     <h5 className="card-title">Dev Team Builder</h5>
                     <p>Use command line to build and generate template of Dev Team</p>
@@ -40,7 +44,7 @@ function ProjectCard() {
             </div>
 
             <div className="card" style={{width: "36rem"}}>
-                <img src="/EmployeeGenerator.png" className="card-img-top" alt="developer team builder" />
+                <img rc={TeamBuilder} className="card-img-top" alt="developer team builder" />
                 <div className="card-body">
                     <h5 className="card-title">Dev Team Builder</h5>
                     <p>Use command line to build and generate template of Dev Team</p>
