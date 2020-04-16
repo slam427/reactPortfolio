891f7fe87b3224e3b12eefddbf5a29025b165a11
Steven Lam <stevenvl@gmail.com>
2020-04-15 23:42:08 -0400

    components built

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,7 +12,18 @@ function App() {
   return (
     <Router>
     <div>
-    HELLO WORLD
+    <Nav />
+    <Switch>
+      <Route path='/'>
+        <Main />
+      </Route>
+      <Route path='/portfolio'>
+        <Portfolio />
+      </Route>
+      <Route path='/contact'>
+        <Contact />
+      </Route>
+    </Switch>
   </div>
 </Router>
 
